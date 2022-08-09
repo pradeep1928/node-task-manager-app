a6711c876c1e256a8bfc7e03e37a2179c5874acc
pradeep <ppadmukhi28@gmail.com>
2022-08-09 14:00:35 +0530

    root route added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const app = require('./app')
 const port = process.env.PORT;
 
+app.get('/', (req, res) => {
+  res.send("Welcome to task-manager-app")
+})
+
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
 });
